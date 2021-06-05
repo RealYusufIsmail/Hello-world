3f0426fdc1f2e30e7072b37950b012fc6c232ec9
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-05 18:54:17 +0100

    added hello world for java 16

---
+++ java 16/HelloWorld/src/com/yusuf/Main.java
@@ -0,0 +1,9 @@
+package com.yusuf;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello From Java "+ args[0] + "!");
+    }
+}
---
+++ java 16/HelloWorld/test.java
@@ -0,0 +1,5 @@
+String s1 = "hello world";
+s1
+var  i1 = 21;
+var i2 = 33;
+var result = i1 * i2;
